e1dcd2382bdd46dfe389807aaade0248dcffe36f
Mahmoud-A-Yehia <mahmoud.am.yehia@gmail.com>
2024-08-14 14:48:42 +0300

    first

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+// const BookRouter = require('./routes/books.js');
+import booksRoute from './routes/books.js';
 
 import connectDB from "./config/db.js"
 
@@ -15,10 +17,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+app.use('/books', booksRoute);
 
 
 app.get('/',(req,res)=>{
-    res.send('mahmoudismail')
+    res.send('Server is running')
 })
 
 app.listen(Port,()=>console.log(`server running on port: ${Port}`))
