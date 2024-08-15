789dba3b074dd1bd5c218905bd62dd87d41454bd
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-15 12:16:42 +0300

    edit : database connection to be in book-store instead of test

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 let MONGO_URI =
-  "mongodb+srv://MARNBookStore:4Hf89zYymjHX9jx4@bookstoreapp.ks6he.mongodb.net/?retryWrites=true&w=majority&appName=BookStoreApp";
+  "mongodb+srv://MARNBookStore:4Hf89zYymjHX9jx4@bookstoreapp.ks6he.mongodb.net/book-store?retryWrites=true&w=majority&appName=BookStoreApp";
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
