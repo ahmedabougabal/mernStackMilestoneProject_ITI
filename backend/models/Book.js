a718ce93bf223746548467c6fab6170abfe2f747
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-18 19:12:39 +0300

    author.js : add function to Get all books by a specific author

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -5,8 +5,15 @@ const schema = new mongoose.Schema({
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Author",
+  name: { type: String, required: true },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
     required: true,
   },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
@@ -14,7 +21,7 @@ const schema = new mongoose.Schema({
   },
   avgRating: { type: Number, default: 0 },
   ratingsCount: { type: Number, default: 0 },
-  image: { type: String },
+  image: String,
 });
 
 export const Book = mongoose.model("Book", schema);
