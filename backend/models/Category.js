05ce829e8916eabc0f47e0842bb8a84cdcf7dae0
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-18 19:16:41 +0300

    update : CategorySchema

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true }
 });
 
 export const Category = mongoose.model("Category", CategorySchema);
