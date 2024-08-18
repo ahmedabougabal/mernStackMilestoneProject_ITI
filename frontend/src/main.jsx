06f958f16ba5aa20a98a2dfeb6d83942f26d98ac
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-19 00:57:37 +0300

    &categories: Auto stash before merge of "authors&categories" and "origin/authors&categories"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {React} from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
