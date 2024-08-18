2d6a4b1ff052fd702073b99909ff6365ecbc7ee2
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-18 23:55:32 +0300

    add axios to app.jsx and add cors to index.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
+import './app.css';
 import axios from 'axios';
-import { Routes , Route} from 'react-router';
-import Home from './pages/Home'
-
+// import { response } from 'express';
 
 
 function App() {
@@ -13,11 +12,17 @@ function App() {
     )
   },[])
 
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
 
-    </Routes>
+  useEffect(()=>{
+    axios.get('http://localhost:5200/books').then(
+      response => console.log(response)
+    )
+  },[])
+
+  return (
+    <div className='app'>
+      <h1>testing the program</h1>
+    </div>
   )
 }
 
