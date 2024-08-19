0291dd76755a0a901711c6607fddab9c234610bf
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-19 21:12:09 +0300

    add CategoryList.jsx

---
+++ frontend/src/components/CategoryList.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('http://localhost:5200/categories');
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  return (
+    <div>
+      <h2>Category List</h2>
+      <ul>
+        {categories.map(category => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
