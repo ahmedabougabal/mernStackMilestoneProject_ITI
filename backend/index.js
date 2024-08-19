30429f70c01a539ae4aa713f8459f7dbb4cc5bba
mahm0udismail <mahmoudismail9114@gmail.com>
2024-08-19 22:17:03 +0300

    addToRead

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,11 @@ import cors from "cors"; // Import CORS to solve the problem between front and b
 import booksRoute from "./routes/books.js";
 import authorRoutes from "./routes/authors.js";
 import categoryRoutes from "./routes/categories.js";
+import  listRoutes  from "./routes/list.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
-const Port = process.env.PORT || 5200;
+const Port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -33,6 +34,7 @@ app.use(
 app.use("/", booksRoute); // Use the books routes
 app.use("/", authorRoutes); // This prefixes all routes in `authors.js` with `/api`
 app.use("/", categoryRoutes);
+app.use("/", listRoutes);
 
 app.get("/", (req, res) => {
     res.send("Server is running");
--- backend/models/modelListRead.js
+++ backend/models/modelListRead.js
@@ -1,34 +1,30 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-    name: { type: String, required: true },
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Author",
-      required: true,
-    },
-    Book: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Book",
-        required: true,
-      },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-      },
-    status: {
-        type: String,
-        enum: ['read', 'reading', 'want to read'],  // The status field can only have these values
-        required: true,  // Makes the field required
-        default: 'want to read',  // Optional: set a default value
-    },
-      timestamps: true,
+  name: { type: String, required: true },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['read', 'reading', 'want to read'],
+    required: true,
+    default: 'want to read',
+  },
+}, {
+  timestamps: true,  // Enable timestamps (createdAt and updatedAt fields)
+});
 
-  });
-  
-  export const Book = mongoose.model("listRead", schema);
-
-
-
-shelve
+export const listRead = mongoose.model("listRead", schema);
