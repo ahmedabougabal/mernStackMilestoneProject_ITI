48f5cd91ef045ef4b7287df240492c7df9084ac4
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-19 21:19:55 +0300

    update app.jsx and vite.confige files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-import { Routes , Route} from 'react-router';
-import Home from './pages/HomePage'
-
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+// import LoginPage from './pages/LoginPage';
+// import RegisterPage from './pages/RegisterPage';
+import BookDetails from './components/BookDetails';
+import AuthorDetails from './components/AuthorDetails';
 
 function App() {
-
-  useEffect(()=>{
-    axios.get('http://localhost:5200/books').then(
-      response => console.log(response)
-    )
-  },[])
-
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-
-    </Routes>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} /> */}
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/author/:id" element={<AuthorDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
