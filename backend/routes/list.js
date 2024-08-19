a389336c3ebf5844a3073b5cf2e8080f020e9cbe
mahm0udismail <mahmoudismail9114@gmail.com>
2024-08-19 19:48:17 +0300

    add get allList and userlist

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -3,3 +3,22 @@ import { listRead } from "../models/modelListRead";
 
 const router = express.Router();
 
+
+
+router.get("/getAllList", async (request, response) => {
+    try {
+      const list = await listRead.find({}).populate("user", "id username");
+      res.json(list);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
+router.get("/getUserList", async (request, response) => {
+    try {
+      const UserList = await listRead.find({ user: req.user._id });
+      res.json(UserList);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
