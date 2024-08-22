ff81417c21ea272e6817e65307e6c4750639b11a
ahmedabougabal <ahmedabougabal@live.com>
2024-08-22 22:22:02 +0300

    feat: implemented getAllUsers, getCurrentUserProfile and updateCurrentUserProfile

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,39 +1,36 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/userModel'
-import asyncHandler from './asyncHandler'
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import asyncHandler from "./asyncHandler.js";
 
 // will check if the token is valid for login and logout 
-const authenticate = asyncHandler(async(req,res, next)=> {
-  //* will read the JWT from the 'jwt cookie'
-  let token ;
-  token = req.cookie.jwt
+//* will read the JWT from the 'jwt cookie'
+const authenticate = asyncHandler(async (req, res, next) => {
+  let token;
 
-  if(token){
+  // Read JWT from the 'jwt' cookie
+  token = req.cookies.jwt;
+
+  if (token) {
     try {
-      const decoded =jwt.verify(token, process.env.JWT_SECRET)
-      req.user = await User.findById(decoded.userId).select('-password')
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
-      res.status(401)
-      throw new Error("not authorized, token failed")
+      res.status(401);
+      throw new Error("Not authorized, token failed.");
     }
-  }else{
-    res.status(401)
-    throw new Error("not authorized,no token ")
-
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token.");
   }
-})
+});
 
-// check for admin ?
-
-const authorizeAdmin = (req,res, next)=>{
-  if(req.user && req.user.isAdmin){
-    next()
+const authorizeAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send("Not authorized as an admin.");
   }
-  else
-  {
-    res.status(401).send("not authorized as annn admin")
-  }
-}
+};
 
-export {authenticate,authorizeAdmin};
+export { authenticate, authorizeAdmin };
