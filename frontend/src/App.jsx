bf15620a33b36573051978dfe9adc46c20f17b9c
ahmedabougabal <ahmedabougabal@live.com>
2024-08-26 10:57:29 +0300

    fix : Uncaught Error: You cannot render a <Router> inside another <Router> in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import React from 'react';
 import './index.css';
 import 'tailwindcss/tailwind.css';
 
 import Navbar from './components/Navbar'; 
-import Footer from './components/Footer'; 
+import Footer from './components/Footer';
 
 import HomePage from './pages/HomePage';
-import HomeBook from './components/HomeBook';
 import Shelf from './components/Shelf';
 import AuthorList from './components/AuthorList';
 import AuthorDetails from './components/AuthorDetails';
 import CategoryList from './components/CategoryList';
 import CategoryDetails from './components/CategoryDetails';
 import BookList from './components/BookList';
-import BookDetails from './components/BookDetails.jsx';
+import BookDetails from './components/BookDetails';
 
+import HomeBook from './components/HomeBook';
 import USauthorList from './components/USauthorList';
 import UScategoryList from './components/UScategoryList';
 
@@ -26,38 +25,34 @@ import UserProfile from './components/UserProfile';
 import AdminProfile from './components/AdminProfile';
 import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {  
+function App() {
   return (
     <Router>
-      <div>
-        <Navbar /> 
-        <main className="p-4">
-          <Routes>
-            {/* Public routes */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="/Shelf" element={<Shelf />} /> 
-            <Route path="/authors" element={<AuthorList />} />
-            <Route path="/authors/:id" element={<AuthorDetails />} />
-            <Route path="/books" element={<BookList />} />
-            <Route path='/books/details/:id' element={<BookDetails />} />
-            <Route path="/categories" element={<CategoryList />} />
-            <Route path="/categories/:id" element={<CategoryDetails />} />
+      <Navbar />
+      <main className="p-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Shelf" element={<Shelf />} /> 
+          <Route path="/authors" element={<AuthorList />} />
+          <Route path="/authors/:id" element={<AuthorDetails />} />
+          <Route path="/books" element={<BookList />} />
+          <Route path='/books/details/:id' element={<BookDetails />} />
+          <Route path="/categories" element={<CategoryList />} />
+          <Route path="/categories/:id" element={<CategoryDetails />} />
 
-            {/* Users' specific routes */}
-            <Route path="/USauthorList" element={<USauthorList />} />
-            <Route path="/UScategoryList" element={<UScategoryList />} />
+          {/* User pages */}
+          <Route path="/" element={<HomeBook />} />
+          <Route path="/USauthorList" element={<USauthorList />} />
+          <Route path="/UScategoryList" element={<UScategoryList />} />
 
-            {/* Authentication routes */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-
-            {/* Protected routes */}
-            <Route path="/user-profile" element={<ProtectedRoute role="user"><UserProfile /></ProtectedRoute>} />
-            <Route path="/admin-profile" element={<ProtectedRoute role="admin"><AdminProfile /></ProtectedRoute>} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+          {/* Authentication pages */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user-profile" element={<ProtectedRoute role="user"><UserProfile /></ProtectedRoute>} />
+          <Route path="/admin-profile" element={<ProtectedRoute role="admin"><AdminProfile /></ProtectedRoute>} />
+        </Routes>
+      </main>
+      <Footer />
     </Router>
   );
 }
